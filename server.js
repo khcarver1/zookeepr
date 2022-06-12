2e285e7e309131e740f48116d72fb8bc8272e162
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-06-12 15:51:49 -0600

    updates

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -40,7 +41,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
 }
-
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -48,7 +52,15 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
